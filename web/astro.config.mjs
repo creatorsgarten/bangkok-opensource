6b52e20f981e10fe90b9a61b8679f63ccfa737e5
Phoomparin Mano <poom@hey.com>
2022-12-31 01:24:38 +0700

    feat: setup tailwind, mdx and svelte

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), svelte(), mdx()]
+});
